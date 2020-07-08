e79877bba72f317a5c3d0a1e900c36bb511cfdac
Sunny Rekhi <rohit.rekhi@gmail.com>
2020-07-07 23:17:11 -0700

    Toggle time display next to icon

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
-const { app, Menu, Tray, BrowserWindow } = require('electron')
 const path = require("path")
+const { app, Menu, Tray } = require('electron')
+
 const alarmPng = path.join(__dirname, "images/alarm.png")
-const sleep = path.join(__dirname, "images/sleep.png")
-
-var timeElapsed = 0;
-var timerID = -1;
-
-function tick() {
-    timeElapsed++
-    document.getElementById("timer").innerHTML = timeElapsed;
-}
-
-function start() {
-    if(timerID == -1){
-        timerID = setInterval(tick, 1000);
-    }
-}
-
-function stop() {
-    if(timerID != -1){
-        clearInterval(timerID)
-        timerID = -1
-    }
-}
-
-function reset() {
-    stop();
-    timeElapsed = -1;
-    tick()
-}
+const sleepPng = path.join(__dirname, "images/sleep.png")
 
 app.on('ready', () => {
   app.dock.hide()
-  const tray = new Tray(alarmPng)
+  const tray = new Tray(sleepPng)
+
+  let timeElapsed = 0;
+  let timerID = -1;
+  let showRunningTime = false; 
+
+  tick = () => {
+    timeElapsed++
+    if (showRunningTime) { 
+        const timeStr = new Date(timeElapsed * 1000).toISOString().substr(11, 8);
+        tray.setTitle(timeStr);
+    }
+  }
+
   startTimer = () => { 
     tray.setImage(alarmPng)
+    if(timerID == -1){
+      timerID = setInterval(tick, 1000);
+    }
   }
 
   stopTimer = () => { 
-    tray.setImage(sleep)
+    tray.setImage(sleepPng)
+    if(timerID != -1) {
+      clearInterval(timerID)
+      timerID = -1
+    }
   }
 
-  seeTime = () => {
-    const win = new BrowserWindow({ width: 800, height: 600 })
-    win.loadURL(`file://${__dirname}/index.html}`)
+  toggleTimeDisplay = () => {
+      showRunningTime = !showRunningTime
+      if (!showRunningTime) { 
+        tray.setTitle("")
+      }
+  }
+
+  resetTimer = () => {
+    stopTimer();
+    timeElapsed = -1;
+    tick()
   }
   
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Start time', type: 'radio', click: startTimer },
-    { label: 'Stop time', type: 'radio', click: stopTimer },
-    { label: 'How long have I worked?', type: 'normal', click: seeTime },
+    { label: 'Start time', type: 'radio', click: startTimer  },
+    { label: 'Stop time', type: 'radio', click: stopTimer, checked: true },
+    { label: "Toggle time display", type: 'normal', click: toggleTimeDisplay },
+    { label: 'Reset time', type: 'normal', click: resetTimer },
   ])
-  tray.setContextMenu(contextMenu)
+    tray.setContextMenu(contextMenu)
 });
